ad30fda14a50624b9069355f2d94fa7c3e9fab08
Luis <kilinkala@gmail.com>
2015-06-25 11:55:26 +0200

    Resize images

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,9 +2,9 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    :aws_access_key_id     => ENV["aws_access_key_id"],                        # required
-    :aws_secret_access_key => ENV ["aws_secret_access_key "]'8523tlK0Ew9iURRqXWoqglMoJjfjcB7TuTD55zpU',
+    aws_access_key_id:     ENV["aws_access_key_id"],                        # required
+    aws_secret_access_key: ENV["aws_secret_access_key"],
     region:                'eu-west-1'                        
   }
-  config.fog_directory  = ENV ["config.fog_directory"]                         # required
+  config.fog_directory  = ENV["config.fog_directory"]                         # required
 end
